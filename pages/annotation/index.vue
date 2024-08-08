a34dfdcf4ce37fe1a8441c21faa990a5259a8ae7
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-08-08 21:26:46 +0800

    theme

---
+++ pages/annotation/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+annotation
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/auth/login.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: '',
+})
+</script>
+
+<template>
+login
+</template>
+
+<style scoped>
+
+</style>
