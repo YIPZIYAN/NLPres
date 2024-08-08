be34c8ea9a85e864908edd5eb394c2a5a27a3daf
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-08-08 12:56:44 +0800

    Test Layouts

--- layouts/default.vue
+++ layouts/default.vue
@@ -319,7 +319,7 @@
   <!-- Content -->
   <div class="w-full lg:ps-64">
     <div class="p-4 sm:p-6 space-y-4 sm:space-y-6">
-      <!-- your content goes here ... -->
+      <slot></slot>
     </div>
   </div>
   <!-- End Content -->
