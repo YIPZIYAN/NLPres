44d6c9f20064310290cf0aa4d075ab8be5c5f647
GohChunYen <chunyengoh030503@gmail.com>
2024-08-15 02:19:06 +0800

    File Format UI

---
+++ components/FileUpload.vue
@@ -0,0 +1,26 @@
+<script setup lang="js">
+import PFileUpload from 'primevue/fileupload';
+
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+
+const onAdvancedUpload = () => {
+  toast.add({ severity: 'info', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
+
+</script>
+
+<template>
+  <div class="card">
+    <Toast />
+    <PFileUpload name="demo[]" url="/api/upload" @upload="onAdvancedUpload($event)" :multiple="true" accept="image/*" :maxFileSize="1000000">
+      <template #empty>
+        <span>Drag and drop files to here to import.</span>
+      </template>
+    </PFileUpload>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/format-conversion/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Select/>
+<FileUpload/>
+  <UButton
+      size="md"
+      color="blue"
+      variant="solid"
+      label="Import"
+      :trailing="false"
+  />
+</template>
+
+<style scoped>
+
+</style>
