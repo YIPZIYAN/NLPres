4492c6d068cc2267bd9783c79862f0e780cebb7a
GohChunYen <chunyengoh030503@gmail.com>
2024-08-18 20:35:19 +0800

    Export Dataset UI

---
+++ pages/dataset/export.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const formatSelections = [{
+  key: '.txt',
+  label: 'TXT',
+  value: 'https://cdn-icons-png.freepik.com/512/11580/11580874.png?ga=GA1.1.1429049260.1723829582'
+}, {
+  key: '.json',
+  label: 'JSON',
+  value: 'https://cdn-icons-png.freepik.com/512/11580/11580838.png?ga=GA1.1.1429049260.1723829582'
+}, {
+  key: '.jsonl',
+  label: 'JSONL',
+  value: 'https://cdn-icons-png.freepik.com/512/11580/11580838.png?ga=GA1.1.1429049260.1723829582'
+}, {
+  key: '.csv',
+  label: 'CSV',
+  value: 'https://cdn-icons-png.freepik.com/512/11580/11580842.png?ga=GA1.1.1429049260.1723829582'
+}, {
+  key: '.xml',
+  label: 'XML',
+  value: 'https://cdn-icons-png.freepik.com/512/11580/11580864.png?ga=GA1.1.1429049260.1723829582'
+}, {
+  key: '.conllu',
+  label: 'CONLL-U',
+  value: 'https://cdn-icons-png.freepik.com/512/16961/16961685.png?ga=GA1.1.1429049260.1723829582'
+}]
+
+const selectedFormat = ref('')
+</script>
+
+<template>
+  <div class="space-y-6">
+    <USelectMenu
+        size="xl"
+        placeholder="File Format"
+        color="blue"
+        variant="outline"
+        :options="formatSelections"
+        v-model="selectedFormat"
+    />
+
+    <UCheckbox color="blue" label="Export only approved datasets" />
+
+    <UButton
+        :block="false"
+        :disabled="!selectedFormat"
+        color="blue"
+        variant="solid"
+        label="Export"
+
+    />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
