11a39f20ffa751e6d5b4465a70bbd009c488e0f1
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-09-29 16:24:33 +0800

    add IAA

---
+++ pages/project/[id]/evaluation/index.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import type {Avatar} from "#ui/types";
+
+const iaaMetrics = ['Cohen Kappa']
+
+
+const users = ['YIP ZI YAN', 'GOH CHUN YEN']
+const profiles = [{
+  id: 1,
+  name: "YIP ZI YAN",
+  avatar: "https://avatars.githubusercontent.com/u/739984?v=4",
+}, {
+  id: 1,
+  name: "GOH CHUN YEN",
+}];
+
+const selectedMetric = ref()
+const selectedUsers = ref([])
+
+</script>
+
+<template>
+
+  <div class="space-y-4">
+    <UFormGroup label="Select annotators">
+      <USelectMenu
+          v-model="selectedUsers"
+          :options="users"
+          multiple
+          searchable
+          searchable-placeholder="Search a person..."
+          size="xl"
+          variant="outline"
+          placeholder="Select annotators">
+      </USelectMenu>
+    </UFormGroup>
+    <UFormGroup label="Select an Inter-annotator Agreement (IAA) Metric">
+      <USelectMenu
+          size="xl"
+          placeholder="Select an IAA Metric"
+          variant="outline"
+          :options="iaaMetrics"
+          v-model="selectedMetric"
+      />
+    </UFormGroup>
+  </div>
+  <div class="flex justify-end">
+    <UButton
+        color="blue"
+        variant="solid"
+        label="Evaluate"
+    />
+  </div>
+  <div>
+    <h1 class="m-2">Result: 0.88</h1>
+    <div>
+      <UCard v-for="user in profiles" :key="user.id">
+        <template #header>
+          <div class="flex items-center">
+            <UAvatar
+                size="lg"
+                :src=user?.avatar
+                :alt=user?.name
+            />
+            <div class="ml-4 truncate">
+              <p class="truncate">{{ user.name }}</p>
+            </div>
+          </div>
+        </template>
+      </UCard>
+    </div>
+
+  </div>
+
+
+</template>
