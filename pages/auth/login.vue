c6f8760792b742e831acef8b131103b4dc6d0700
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-10-04 20:40:08 +0800

    register

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -2,6 +2,8 @@
 definePageMeta({
   layout: 'auth',
 })
+const showPass = ref(false);
+const password = '';
 </script>
 
 <template>
@@ -10,14 +12,28 @@ definePageMeta({
   </h1>
   <form class="space-y-4 md:space-y-6" action="#">
     <UFormGroup label="Email">
-      <UInput placeholder="Enter your email" icon="i-heroicons-envelope" />
+      <UInput placeholder="Enter your email" icon="i-heroicons-envelope"/>
     </UFormGroup>
     <UFormGroup label="Password" name="password">
-      <UInput placeholder="Enter your password" type="password" icon="i-heroicons-lock-closed" />
+      <UInput placeholder="Enter your password"
+              v-model="password"
+              :ui="{ icon: { trailing: { pointer: '' } } }"
+              :type="showPass ? 'text' : 'password'"
+              icon="i-heroicons-lock-closed">
+        <template #trailing>
+          <UButton
+              color="gray"
+              variant="link"
+              icon="i-heroicons-eye"
+              :padded="false"
+              @click="showPass = !showPass"
+          />
+        </template>
+      </UInput>
     </UFormGroup>
     <div class="flex items-center justify-between">
       <div class="flex items-start">
-        <UCheckbox name="remember" label="Remember me" />
+        <UCheckbox name="remember" label="Remember me"/>
       </div>
       <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
         password?</a>
@@ -26,9 +42,12 @@ definePageMeta({
       Sign in
     </UButton>
     <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-      Don’t have an account yet? <a href="#"
-                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
-      up</a>
+      Don’t have an account yet?
+      <NuxtLink
+          to="register"
+          class="font-medium text-primary-600 hover:underline dark:text-primary-500">
+        Sign up
+      </NuxtLink>
     </p>
   </form>
 </template>
