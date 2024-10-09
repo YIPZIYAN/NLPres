35da1fe3659b66c57cf075c909badec3e4ec0f0c
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-10-09 16:19:34 +0800

    unauthenticatedOnly for login and register

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 definePageMeta({
   layout: 'auth',
+  auth: {
+    unauthenticatedOnly: true,
+  },
 })
 
 const {signIn} = useAuth()
--- pages/register.vue
+++ pages/register.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 definePageMeta({
   layout: 'auth',
-  auth:false,
+  auth: {
+    unauthenticatedOnly: true,
+  },
 })
 
 const showPass = ref(false);
