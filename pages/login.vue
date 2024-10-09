13a44a528d1ba7d3846ed8e239a8cbd8c3375a6c
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-10-09 17:02:34 +0800

    login validation

--- pages/login.vue
+++ pages/login.vue
@@ -6,6 +6,16 @@ definePageMeta({
   },
 })
 
+import {object, string, type InferType} from 'yup'
+import type {FormSubmitEvent} from '#ui/types'
+
+const schema = object({
+  email: string().email('Please enter a valid email address.').required('Please enter your email address.'),
+  password: string().required('Please enter your password.'),
+})
+
+type Schema = InferType<typeof schema>
+
 const {signIn} = useAuth()
 const showPass = ref(false);
 
@@ -14,35 +24,41 @@ const formData = reactive({
   password: '',
 })
 
-const login = async (e) => {
+const error = ref();
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
   try {
-    e.preventDefault()
-    let res = await signIn(
-        {...formData},
+    await signIn(
+        {...event.data},
         {callbackUrl: "/project"}
     )
-
-    console.log("res", res);
-
-  } catch (error) {
-    console.log("error", error);
-    console.log(formData);
+  } catch (e) {
+    error.value = e.response;
   }
 }
+
 </script>
 
 <template>
   <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
     Login
   </h1>
-  <form class="space-y-4 md:space-y-6" @submit.prevent="login" method="post">
-    <UFormGroup label="Email">
-      <UInput placeholder="Enter your email" name="email"
+  <UAlert v-if="error"
+          color="red"
+          variant="solid"
+          title="Opps.."
+          description="Please check your credentials and try again."/>
+
+  <UForm :schema="schema"
+         :state="formData"
+         class="space-y-4 md:space-y-6"
+         @submit="onSubmit" method="post">
+    <UFormGroup label="Email" name="email">
+      <UInput placeholder="Enter your email"
               v-model="formData.email" icon="i-heroicons-envelope"/>
     </UFormGroup>
-    <UFormGroup label="Password">
+    <UFormGroup label="Password" name="password">
       <UInput placeholder="Enter your password"
-              name="password"
               v-model="formData.password"
               :ui="{ icon: { trailing: { pointer: '' } } }"
               :type="showPass ? 'text' : 'password'"
@@ -76,5 +92,5 @@ const login = async (e) => {
         Sign up
       </NuxtLink>
     </p>
-  </form>
+  </UForm>
 </template>
